84b60cf8c8f4abfe9c387dc94dd65b7730b09efa
Enrique Romero <enrique.romero1110@gmail.com>
2023-01-25 18:48:58 -0500

    debugging

--- Storm Warning.c
+++ Storm Warning.c
@@ -17,25 +17,10 @@ struct ArrayList {
     int size, capacity;
 };
 
-// Given a point to a list initialize the list
+// Prototypes
 ArrayList * initArrayList();
-
-// Given a list and a boat to add to it, add the boat to the end
-// of the list
-// Expand the list if necessary
 void appendToList(ArrayList * list, Boat newBoat);
-
-// Given
-// * the x and y location of a storm,
-// * the dimensions of the storm,
-// * the dimensions of the world,
-// * the list of ships under consideration,
-// * the time change since the beginning of the program,
-// * and a flag whether boats should be printed or not
-// Return the number of boats in the storm and the list
-// Print the names of the boats if the should_print flag is non-zero
 int query(int x, int y, int storm_width, int storm_height, int world_width, int world_height, ArrayList * list, int cur_time, int should_print);
-
 void freeMemory(ArrayList * list);
 
 // The main function
@@ -71,7 +56,7 @@ int main() {
             scanf("%d", &newBoat.start_x);
             scanf("%d", &newBoat.start_y);
             scanf("%c", &direction);
-            scanf("%c", newBoat.name);
+            scanf("%s", newBoat.name);
 
             // Zero index my values
             newBoat.start_x -= 1;
@@ -151,6 +136,8 @@ int main() {
                                     height, rows, 
                                     currTime, should_print);
 
+            printf("%d\n", totalAffected);
+
             // set query function to print
             should_print = 1;
 
@@ -177,6 +164,7 @@ int main() {
     return 0;
 }
 
+// Given a point to a list initialize the list
 ArrayList * initArrayList() {
     ArrayList * list = (ArrayList *)malloc(sizeof(ArrayList));
     
@@ -188,11 +176,14 @@ ArrayList * initArrayList() {
     return list;
 }
 
+// Given a list and a boat to add to it, add the boat to the end
+// of the list
+// Expand the list if necessary
 void appendToList(ArrayList * list, Boat newBoat) {
     // Check if list should be expanded
     if(list->size == list->capacity) {
         list->capacity *= 2;
-        list->array = realloc(list->array, list->capacity * sizeof(Boat));
+        list->array = (Boat *)realloc(list->array, list->capacity * sizeof(Boat));
     }
 
     // Add value to end
@@ -200,15 +191,28 @@ void appendToList(ArrayList * list, Boat newBoat) {
     list->size++;
 }
 
+// Given
+// * the x and y location of a storm,
+// * the dimensions of the storm,
+// * the dimensions of the world,
+// * the list of ships under consideration,
+// * the time change since the beginning of the program,
+// * and a flag whether boats should be printed or not
+// Return the number of boats in the storm and the list
+// Print the names of the boats if the should_print flag is non-zero
 int query(int x, int y, int storm_width, int storm_height, int world_width, int world_height, ArrayList * list, int cur_time, int should_print) {
     int affectedShips = 0;          // Tracks affected ships
     int shift, newPos, stormPos;    // Tracks ship and storm positions
-    int stormWrap = -1;             // Tracks if storm wraps
+    int stormWrap = -5;             // Tracks if storm wraps
 
+    printf("list size %d", list->size);
     for(int i = 0; i < list->size; i++) {
-        if(list->array[i].start_time < cur_time) {            
+        printf("boat %d start time: %d\n", i, list->array[i].start_time);
+        if(list->array[i].start_time <= cur_time) {   
+            printf("Found valid start time\n");     
             // Row check
             if(list->array[i].delta_x != 0) {
+                printf("started row check\n");
                 //Check if the ship is in the same row as the storm
                 for(int j = 0; j < storm_height; j++) {
                     if(list->array[i].start_y == storm_height + j) {
@@ -220,12 +224,19 @@ int query(int x, int y, int storm_width, int storm_height, int world_width, int 
                         if(newPos > world_width || newPos < 0)
                              newPos %= world_width;
 
+                        printf("Ships current location is: %d\n", newPos);
+
                         // Get storm's furthest position
                         stormPos = x + storm_width;
 
                         // Check if storm wraps
-                        if(stormPos >= world_width)
-                            stormWrap = world_width - stormPos;
+                        if(stormPos >= world_width) {
+                            stormWrap = stormPos - world_width - 1;
+                            stormPos = world_width - 1;
+                        }
+
+                        printf("stormPos is: %d", stormPos);
+                        printf("stormWrap is: %d", stormWrap);
 
                         // Check if boat is in storm
                         if((newPos > x && newPos < stormPos) || newPos < stormWrap) {
@@ -234,16 +245,58 @@ int query(int x, int y, int storm_width, int storm_height, int world_width, int 
                             // print ship if needed
                             if(should_print == 1)
                                 printf("%s\n", list->array[i].name);
-                        }
 
+                            printf("Ship is in storm\n");
+                        }
                     }
                 }
             }
 
+            printf("finished row check\n");
+
             // Column check
             if(list->array[i].delta_y != 0) {
+                printf("started column check\n");
+                //Check if the ship is in the same column as the storm
+                for(int j = 0; j < storm_width; j++) {
+                    if(list->array[i].start_x == storm_width + j) {
+                        // Get boats current location
+                        shift = (cur_time - list->array[i].start_time) * list->array[i].delta_y;
+                        newPos = (shift + list->array[i].start_y);
+                        
+                        // Wrap ship if needed
+                        if(newPos > world_height || newPos < 0)
+                             newPos %= world_height;
 
+                        printf("Ships current location is: %d\n", newPos);
+
+                        // Get storm's furthest position
+                        stormPos = y + storm_height;
+
+                        // Check if storm wraps
+                        if(stormPos >= world_height) {
+                            stormWrap = stormPos - world_height - 1;
+                            stormPos = world_height - 1;
+                        }
+
+                        printf("stormPos is: %d", stormPos);
+                        printf("stormWrap is: %d", stormWrap);
+
+                        // Check if boat is in storm
+                        if((newPos > y && newPos < stormPos) || newPos < stormWrap) {
+                            affectedShips++;
+                            
+                            // print ship if needed
+                            if(should_print == 1)
+                                printf("%s\n", list->array[i].name);
+                            
+                            printf("Ship is in storm\n");
+                        }
+                    }
+                }
             }
+
+            printf("finished column check\n");
         }
     }
 
@@ -256,47 +309,3 @@ void freeMemory(ArrayList * list) {
     free(list->array);
     free(list);
 }
-
-// query() comments
-    // Create and initialize the answer
-    // Loop through all the boats in the list
-        // Get the current boat
-        // Get the current location of the boat (based on time)
-        // Find the offset of the boat from the storm
-        // Wrap the offsets around the world if necessary (modulo)
-        // Check if the boat is in the storm by using the offset
-
-// main() comments
-    // Read the size of the world
-    // Create the array of array lists for x and y world segments
-    // Loop through and initialize all the array lists from the two arrays
-    // Initialize the current time
-    // Read the first command
-    // Loop until the exit command is given
-        // Determine the command type
-            // ADD COMMAND
-                // Create a static boat
-                // Read in the boat values (be careful with the direction)
-                // Convert the x and y to zero indexed values
-                // Initialize the delta x and y based on the direction
-                // Initialize the time the boat arrived
-                // Check if boat is Left/Right VS Up/Down
-                    // Add boat to the x’s (use append function)
-                // Else
-                    // Add boat to the y’s (use append function)
-            // TIME COMMAND
-                // Update the current time
-            // STORM COMMAND
-                // TODO: left off here
-                // Read in the storm information
-                // Convert the x and y to zero indexed values
-                // Loop through and determine how many ships will be affected
-                    // Update total by querying this particular row list
-                    // Update total by querying this particular column list
-                // Print the total number of affected ships
-                // Loop through and print affected ships
-                    // Print the by querying this particular row list
-                    // Print the by querying this particular column list
-        // Read in the next command
-    // Clean up memory
-    // Exit successfully
